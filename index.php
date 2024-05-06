a64612ab4bffd1623b8e71ff45fbe30388bf2529
Artur <arturumbelinov@gmail.com>
2024-05-06 18:26:36 -0300

    corrigindo a merda dos amiguinhos

--- index.php
+++ index.php
@@ -5,56 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OffersFast</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="insert.php">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz@0,9..40;1,9..40&display=swap"
-        rel="stylesheet">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,700;1,9..40,700&display=swap"
-        rel="stylesheet">
-
-
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- FONTE -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- FONTE -->
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz@0,9..40;1,9..40&display=swap" rel="stylesheet"> <!-- FONTE -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- FONTE -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- FONTE -->
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,700;1,9..40,700&display=swap" rel="stylesheet"> <!-- FONTE -->
+    <link rel="stylesheet" href="style.css"> <!-- CSS -->
+    <link rel="stylesheet" href="insert.php"> <!-- ARQUIVO INSERT -->
 </head>
 
 <body>
-
-<div id="headid">
-
-<div id="menu">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+    <div id="headid">
+        <div id="menu">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            </svg>
         </div>
         <div id="home">
             <div class="text">
                 <h6 href="">Home</h6>
             </div>
-            
             <div class="text">
                 <h6 href="">Produtos <ul>
-                    <a href="cadastrar_produto/cadastroProduto.php"><li>Adicionar Oferta</li></a>
-                </ul></h6>
+                        <a href="cadastrar_produto/cadastroProduto.php">
+                            <li>Adicionar Oferta</li>
+                        </a>
+                    </ul>
+                </h6>
             </div>
-            
-
             <input class="input" name="text" type="text" placeholder="Pesquisar Produto">
-
         </div>
         <div id="icons">
-            
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
-
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v25.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm0 96c61.9 0 112 50.1 112 112v25.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V208c0-61.9 50.1-112 112-112zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"/></svg>
-
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff" d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff" d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v25.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm0 96c61.9 0 112 50.1 112 112v25.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V208c0-61.9 50.1-112 112-112zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z" />
+            </svg>
             <div id="login">
-
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path fill="#ffffff" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+                </svg>
                 <a href="tela_login\login.php">Login</a>
             </div>
-
         </div>
     </div>
 
@@ -62,12 +56,9 @@
         <div class="slogan">
             <h1>Grandes <span>ofertas</span>,</br>
                 em um só lugar!</h1>
-
             <p>Descubra descontos incríveis em um só lugar. Junte-se à nossa comunidade e economize mais!</p>
         </div>
-
         <div>
-
             <div class="box_images1">
                 <img src="./assets/" alt="" style="width: 20%;">
                 <p>Sacolão</p>
@@ -93,96 +84,94 @@
             </div>
         </div>
     </section>
-
-
     <section class="categorias_menu">
-
-        <div class="item_seta esquerda" >
-            <img src="./assets/seta-esquerda.svg" alt="" >
+        <div class="item_seta esquerda">
+            <img src="./assets/seta-esquerda.svg" alt="">
         </div>
-        
+        <div class="container">
         <div class="item_categoria">
             <img src="" alt="">
-            <h2>Alimentos</h2>
+            <button>
+                <span class="button_top">
+                    Alimentos
+                </span>
+            </button>
         </div>
-
         <div class="item_categoria">
             <img src="" alt="">
-            <h2>Frutas</h2>
+            <button>
+                <span class="button_top">
+                    Frutas
+                </span>
+            </button>
         </div>
-
-        
         <div class="item_categoria">
             <img src="" alt="">
-            <h2>Legumes</h2>
+            <button>
+                <span class="button_top">
+                    Legumes
+                </span>
+            </button>
         </div>
-
         <div class="item_categoria">
             <img src="" alt="">
-            <h2>Limpeza</h2>
+            <button>
+                <span class="button_top">
+                    Limpeza
+                </span>
+            </button>
         </div>
-
+        </div>
         <div class="item_seta">
             <img src="./assets/seta-direita.svg" alt="">
         </div>
 
     </section>
-    
+
     <section class="ofertas">
-
         <div class="item_oferta">
-
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
-
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+                <h2>Nome Produto</h2>
+                <p>R$</p>
             </div>
 
             <div class="tipo_produto ">
-
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
             </div>
         </div>
-
         <div class="item_oferta">
 
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/Imagem1.png" alt="">
 
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
+
+
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+
+                <h2>Nome Produto</h2>
+                <p>R$</p>
+
             </div>
 
             <div class="tipo_produto ">
 
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
+
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
+
             </div>
         </div>
 
@@ -190,56 +179,56 @@
         <div class="item_oferta">
 
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/Imagem1.png" alt="">
 
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
+
+
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+
+                <h2>Nome Produto</h2>
+                <p>R$</p>
+
             </div>
 
             <div class="tipo_produto ">
 
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
+
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
+
             </div>
         </div>
 
         <div class="item_oferta">
 
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/Imagem1.png" alt="">
 
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
+
+
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+
+                <h2>Nome Produto</h2>
+                <p>R$</p>
+
             </div>
 
             <div class="tipo_produto ">
 
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
+
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
+
             </div>
         </div>
 
@@ -248,28 +237,28 @@
         <div class="item_oferta">
 
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/Imagem1.png" alt="">
 
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
+
+
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+
+                <h2>Nome Produto</h2>
+                <p>R$</p>
+
             </div>
 
             <div class="tipo_produto ">
 
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
+
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
+
             </div>
         </div>
 
@@ -278,37 +267,28 @@
         <div class="item_oferta">
 
             <div class="img_like">
-            <img src="./assets/Imagem1.png" alt="">
+                <img src="./assets/Imagem1.png" alt="">
 
-            <img src="./assets/heart.svg" alt="" >
+                <img src="./assets/heart.svg" alt="">
             </div>
-            
-          
             <div class="informacoes_item">
-    
-                    <h2>Nome Produto</h2>
-                    <p>R$</p>
-                
+
+                <h2>Nome Produto</h2>
+                <p>R$</p>
+
             </div>
 
             <div class="tipo_produto ">
 
                 <div>
-                <span>Frutas</span>
-                <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
-                 </div>
-               
+                    <span>Frutas</span>
+                    <span style="background-color: rgb(224, 162, 68);">Sacolão</span>
+                </div>
                 <a href=""><button><img src="" alt="">+</button></a>
-                 
             </div>
         </div>
-
-
     </section>
-
-
     <section class="sobre_nos">
-
     </section>
 </body>
 
