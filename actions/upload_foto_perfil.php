560cdc153621804204d89ec8408e9466b6b5bc49
Artur <arturumbelinov@gmail.com>
2024-08-28 17:11:44 -0300

    Melhoria no codigo de upar a foto de perfil e funcao de remover

--- actions/upload_foto_perfil.php
+++ actions/upload_foto_perfil.php
@@ -12,14 +12,37 @@ if (isset($_POST['submit'])) {
         echo "<div class='alert alert-danger' role='alert'>Por favor, selecione uma foto!</div>";
     } else {
         $id_usuario = $_SESSION['id_usuario'];
-        $foto_nome = $_FILES["foto_perfil"]["name"];
+        $foto_nome = preg_replace("/[^a-zA-Z0-9.]/", "_", basename($_FILES["foto_perfil"]["name"]));
         $foto_destino = "../uploads_img/" . $foto_nome;
         $foto_destino_novo = str_replace("../", "", $foto_destino);
 
+        // Verificar se o diretório de uploads existe, se não, cria
         if (!file_exists('../uploads_img')) {
             mkdir('../uploads_img', 0777, true);
         }
 
+        // Verificar se o arquivo é uma imagem
+        $check = getimagesize($_FILES["foto_perfil"]["tmp_name"]);
+        if ($check === false) {
+            echo "<div class='alert alert-danger' role='alert'>O arquivo não é uma imagem.</div>";
+            exit();
+        }
+
+        // Verificar o tamanho do arquivo (por exemplo, máximo de 2MB)
+        if ($_FILES["foto_perfil"]["size"] > 2 * 1024 * 1024) {
+            echo "<div class='alert alert-danger' role='alert'>O arquivo é muito grande. Máximo permitido é 2MB.</div>";
+            exit();
+        }
+
+        // Permitir somente certos formatos
+        $allowed_types = ['jpg', 'jpeg', 'png', 'gif'];
+        $file_ext = strtolower(pathinfo($foto_nome, PATHINFO_EXTENSION));
+        if (!in_array($file_ext, $allowed_types)) {
+            echo "<div class='alert alert-danger' role='alert'>Somente arquivos JPG, JPEG, PNG e GIF são permitidos.</div>";
+            exit();
+        }
+
+        // Mover o arquivo para o diretório de destino
         if (move_uploaded_file($_FILES["foto_perfil"]["tmp_name"], $foto_destino)) {
             $sql = "UPDATE usuario SET foto_perfil='$foto_destino_novo' WHERE id_usuario = $id_usuario";
             try {
@@ -35,4 +58,3 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-
--- perfil.php
+++ perfil.php
@@ -28,79 +28,60 @@ $foto_perfil = $user_data['foto_perfil']; // Obtém o caminho da foto de perfil
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz@0,9..40;1,9..40&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz@0,9..40;1,9..40&display=swap"
+        rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/perfil.css">
     <title>Offers</title>
-    <style>
-        .cadastro-link {
-            color: black;
-        }
-
-        .password-hidden {
-            filter: blur(5px);
-            -webkit-filter: blur(5px);
-            user-select: none;
-            pointer-events: none;
-        }
-
-        .toggle-link {
-            cursor: pointer;
-            color: black;
-            text-decoration: none;
-            margin-left: 10px;
-        }
-
-        .edit-name {
-            display: none;
-        }
-    </style>
 </head>
 
 <body>
-<div class="card" style="width: 18rem; text-align: center;">
-    <ul class="list-group list-group-flush">
-        <li>
-            <h1><?php echo $_SESSION['nome']; ?>! Olá, aqui é a sua conta :)</h1>
-        </li>
-        <li class="list-group-item">
-            <form action="actions/upload_foto_perfil.php" method="post" enctype="multipart/form-data">
-                <!-- Bolinha para a foto de perfil com input file -->
-                <div style="margin: 0 auto; width: 100px; height: 100px; border-radius: 50%; overflow: hidden; background-color: #f0f0f0; position: relative;">
-                    <input type="file" name="foto_perfil" id="file-input" style="opacity: 0; width: 100%; height: 100%; position: absolute; top: 0; left: 0; cursor: pointer;">
-                    <img src="<?php echo $foto_perfil; ?>" alt="Foto de Perfil" style="width: 100%; height: 100%; object-fit: cover;">
-                    <div style="position: absolute; bottom: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; padding: 5px; border-radius: 50%;">
-                        <!-- Você pode adicionar ícones ou texto aqui, se desejar -->
+    <div class="card" style="width: 18rem; text-align: center;">
+        <ul class="list-group list-group-flush">
+            <li>
+                <h1><?php echo $_SESSION['nome']; ?>! Olá, aqui é a sua conta :)</h1>
+            </li>
+            <li class="list-group-item">
+                <form action="actions/upload_foto_perfil.php" method="post" enctype="multipart/form-data">
+                    <div class="foto-perfil-container">
+                        <input type="file" name="foto_perfil" id="file-input" class="file-input">
+                        <img src="<?php echo $foto_perfil; ?>" alt="Foto de Perfil" class="foto-perfil-img">
+                        <div class="foto-perfil-text">
+                            <!-- Texto ou ícone opcional -->
+                        </div>
                     </div>
-                </div>
-                <button type="submit" name="submit" class="btn btn-primary mt-2">Salvar Foto</button>
-            </form>
-        </li>
-        <li class="list-group-item">
-            <h3><a href="#" id="cadastro-button" class="cadastro-link">Cadastro</a></h3>
-        </li>
-        <li class="list-group-item" id="dados-conta" style="display: none;">
-            <h3>Dados da conta</h3>
-            <?php 
-            if ($user_data) {
-                echo "<h5>Email:</h5>";
-                echo "<span>" . $user_data["email"] . "</span><br>";
-                echo "<h5>Senha:</h5>";
-                echo "<span id='senha' class='password-hidden'>" . $user_data["senha"] . "</span>";
-                echo "<a href='#' id='toggle-senha' class='toggle-link' onclick='toggleSenha()'>Mostrar senha</a><br>";
-                echo "<a href='alterarSenha.php' id='alterar-senha' class='toggle-link'>Alterar senha</a><br>";
-                echo "<h5>Nome de usuario:</h5>";
-                echo "<span id='nome-usuario'>" . $user_data["nome"] . "</span>";
-                echo "<a href='#' id='editar-nome' class='toggle-link' onclick='mostrarEdicaoNome()'>Editar nome</a><br>";
-                echo "<input type='text' id='input-nome' class='edit-name' value='" . $user_data["nome"] . "' style='display: none;'>";
-                echo "<button id='salvar-nome' class='edit-name' onclick='salvarNome()' style='display: none;'>Salvar</button>";
-            } else {
-                echo "<span>Erro ao buscar dados</span>";
-            }
-            ?>
-        </li>
-    </ul>
-</div>
+                    <button type="submit" name="submit" class="btn btn-primary mt-2">Salvar Foto</button>
+                </form>
+
+                <form action="actions/remover_foto_perfil.php" method="post">
+                    <button type="submit" name="remover_foto" class="btn btn-danger">Remover Foto</button>
+                </form>
+            </li>
+            <li class="list-group-item">
+                <h3><a href="#" id="cadastro-button" class="cadastro-link">Cadastro</a></h3>
+            </li>
+            <li class="list-group-item" id="dados-conta" style="display: none;">
+                <h3>Dados da conta</h3>
+                <?php
+                if ($user_data) {
+                    echo "<h5>Email:</h5>";
+                    echo "<span>" . $user_data["email"] . "</span><br>";
+                    echo "<h5>Senha:</h5>";
+                    echo "<span id='senha' class='password-hidden'>" . $user_data["senha"] . "</span>";
+                    echo "<a href='#' id='toggle-senha' class='toggle-link' onclick='toggleSenha()'>Mostrar senha</a><br>";
+                    echo "<a href='alterarSenha.php' id='alterar-senha' class='toggle-link'>Alterar senha</a><br>";
+                    echo "<h5>Nome de usuario:</h5>";
+                    echo "<span id='nome-usuario'>" . $user_data["nome"] . "</span>";
+                    echo "<a href='#' id='editar-nome' class='toggle-link' onclick='mostrarEdicaoNome()'>Editar nome</a><br>";
+                    echo "<input type='text' id='input-nome' class='edit-name' value='" . $user_data["nome"] . "' style='display: none;'>";
+                    echo "<button id='salvar-nome' class='edit-name' onclick='salvarNome()' style='display: none;'>Salvar</button>";
+                } else {
+                    echo "<span>Erro ao buscar dados</span>";
+                }
+                ?>
+            </li>
+        </ul>
+    </div>
     <script>
         function toggleSenha() {
             var senhaElement = document.getElementById('senha');
@@ -135,7 +116,7 @@ $foto_perfil = $user_data['foto_perfil']; // Obtém o caminho da foto de perfil
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "salvar_nome.php", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.onreadystatechange = function() {
+            xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     document.getElementById('nome-usuario').innerText = novoNome;
                     document.getElementById('nome-usuario').style.display = 'inline';
@@ -149,7 +130,7 @@ $foto_perfil = $user_data['foto_perfil']; // Obtém o caminho da foto de perfil
 
         document.getElementById('senha').classList.add('password-hidden');
 
-        document.getElementById('cadastro-button').addEventListener('click', function() {
+        document.getElementById('cadastro-button').addEventListener('click', function () {
             var dadosConta = document.getElementById('dados-conta');
             if (dadosConta.style.display === 'none') {
                 dadosConta.style.display = 'block';
